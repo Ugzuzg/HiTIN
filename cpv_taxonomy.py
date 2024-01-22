4a938a4c87730ba928ef98da9ae6ba7d86cda4b4
Jarasłaŭ Viktorčyk <ugzuzg@gmail.com>
2024-01-22 20:29:19 +0100

    rename function to tree_to_taxonomy

--- cpv_taxonomy.py
+++ cpv_taxonomy.py
@@ -1,6 +1,6 @@
 import json
 
-def walk_hierarchy(node, depth=0) -> list[str]:
+def tree_to_taxonomy(node) -> list[str]:
     """A taxonomy file is a tab separated file with the following structure:
     Category \t Subcategory1 \t Subcategory2
     Subcategory1 \t Subcategory1.1 \t Subcategory1.2
@@ -13,11 +13,11 @@ def walk_hierarchy(node, depth=0) -> list[str]:
     direct_children = [child['cpv'] for child in (node['children'] or []) if child['cpv'] != node['cpv']]
     out.append('\t'.join([node['cpv']] + direct_children) + '\n')
     for child in (node['children'] or []):
-        out += walk_hierarchy(child, depth=depth+1)
+        out += tree_to_taxonomy(child)
     return out
 
 with open('cpv/cpv_hierarchy.json', 'r') as hierarchy_file:
     with open('cpv/cpv.taxonomy', 'w') as taxonomy_file:
         hierarchy = json.load(hierarchy_file)
-        hierarchy_lines = walk_hierarchy({ 'name': 'Root', 'cpv': 'Root', 'children': hierarchy })
+        hierarchy_lines = tree_to_taxonomy({ 'name': 'Root', 'cpv': 'Root', 'children': hierarchy })
         taxonomy_file.writelines(hierarchy_lines)
