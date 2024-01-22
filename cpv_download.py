34cad415a5cb048b7e1d0f1a06ffbf24355e020c
Julia <juliabukato@gmail.com>
2024-01-22 20:32:13 +0100

    Add comments

--- cpv_download.py
+++ cpv_download.py
@@ -3,14 +3,23 @@ from io import BytesIO
 from zipfile import ZipFile
 from urllib.request import urlopen
 
+# URL to download CPV codes dataset in ZIP format (flat hierarchy)
 URL_CPV_CODES = "https://simap.ted.europa.eu/documents/10184/36234/cpv_2008_xls.zip"
 
+# Fetch CPV codes from the SIMAP website with their names in a given language
 def get_cpv_codes_from_simap(language):
     zipfile = ZipFile(BytesIO(urlopen(URL_CPV_CODES).read()))
     data = pd.read_excel(zipfile.open(zipfile.namelist()[0]))
-    data.rename(columns = {language:'name'}, inplace = True)
+    
+    # Rename the language column to 'name', 
+    # because xls file contains names in different languages and columns are named after each language
+    data.rename(columns={language: 'name'}, inplace=True)
+    
     return data[['CODE', 'name']]
 
+# Fetch CPV codes for English language 
+# and truncate them to the first 8 characters to remove error correction code, which goes after '-'
 df = get_cpv_codes_from_simap('EN')
 df['CODE'] = df['CODE'].apply(lambda x: x[:8])
+
 df.to_csv('cpv/cpv.csv', index=False)
